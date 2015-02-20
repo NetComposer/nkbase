75de4d9f4c5c791cdbb35f46f88a461a245b7190
Carlos González Florido <carlosj.gf@gmail.com>
2015-02-20 10:26:21 +0100

    Update to the last version of riak_core
    Now R17 is supported in riak_core. However pbkdf2 and riak_dt still need to be fixed

--- util/fix_deps_warnings_as_errors.sh
+++ util/fix_deps_warnings_as_errors.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-sed 's/, warnings_as_errors//' deps/poolboy/rebar.config > deps/poolboy/rebar.config.1
-mv deps/poolboy/rebar.config.1 deps/poolboy/rebar.config
+# sed 's/, warnings_as_errors//' deps/poolboy/rebar.config > deps/poolboy/rebar.config.1
+# mv deps/poolboy/rebar.config.1 deps/poolboy/rebar.config
 
-sed 's/warnings_as_errors, //' deps/meck/rebar.config > deps/meck/rebar.config.1
-mv deps/meck/rebar.config.1 deps/meck/rebar.config
+# sed 's/warnings_as_errors, //' deps/meck/rebar.config > deps/meck/rebar.config.1
+# mv deps/meck/rebar.config.1 deps/meck/rebar.config
 
-sed 's/warnings_as_errors, //' deps/riak_core/rebar.config > deps/riak_core/rebar.config.1
-mv deps/riak_core/rebar.config.1 deps/riak_core/rebar.config
+# sed 's/warnings_as_errors, //' deps/riak_core/rebar.config > deps/riak_core/rebar.config.1
+# mv deps/riak_core/rebar.config.1 deps/riak_core/rebar.config
 
 sed 's/{require_min_otp_vsn, "R14B03"}.//' deps/pbkdf2/rebar.config > deps/pbkdf2/rebar.config.1
 mv deps/pbkdf2/rebar.config.1 deps/pbkdf2/rebar.config
